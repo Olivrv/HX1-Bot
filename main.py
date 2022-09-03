1f6c2b1a0e61d2beea4684d866b6c7455b822803
Olivrv <60824785+Olivrv@users.noreply.github.com>
2022-09-03 11:07:52 +0200

    Updates and rework due to deprecated library

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from discord.ext import commands
+import discord
 from discord.ext.commands.errors import CommandNotFound
 import interactions
+import asyncio
+from interactions import presence
 from config import TOKEN
 
 
-help_command = commands.DefaultHelpCommand(
-    no_category='Commands')
 emojis = {1: ":one:", 2: ":two:", 3: ":three:", 4: ":four:", 5: ":five:", 6: ":six:", 7: ":seven:",
           8: ":eight:", 9: ":nine:", 10: ":keycap_ten:"}
 emojis_code = {1: '1️⃣', 2: "2️⃣", 3: "3️⃣", 4: "4️⃣", 5: "5️⃣", 6: "6️⃣", 7: "7️⃣",
                8: "8️⃣", 9: "9️⃣", 10: "🔟"}
-games = ["Centrale", "Les Mines", "L'X", "Démombrer l'infini", "Des maths"]
+games = ["Centrale", "Les Mines", "L'X", "Dénombrer l'infini", "Des maths"]
 other_bot_commands = []
 bot = interactions.Client(token=TOKEN)
 
@@ -20,7 +20,34 @@ async def hello(ctx: interactions.CommandContext):
     await ctx.send(f"Hi there {ctx.author.mention}!")
 
 
-@bot.command()
+@bot.command(name="hack", description="hack a player",
+             options=[interactions.Option(name="player", type=interactions.OptionType.USER,
+                                          required=True, description='player to hack')])
+async def hack(ctx: interactions.CommandContext, player: str):
+    await ctx.send(f"Hacking {player}...")
+    t = 2
+    if ctx.author == player:
+        await ctx.send("Initialising... Wait, no! You can't hack yourself.")
+    elif str(player) == str(bot.me.name):
+        await ctx.send(f"Initialising... Breached {str(bot.me.name)}... Hold on a minute... No!")
+    else:
+        msg = await ctx.send(f"Launching hacking protocols against {player}...")
+        await asyncio.sleep(t)
+        await msg.edit(content="Breaching firewalls...")
+        await asyncio.sleep(t)
+        await msg.edit(content="Breaking developers mind...")
+        await asyncio.sleep(t)
+        await msg.edit(content=f"Looking for {player}'s private folders...")
+        await asyncio.sleep(t)
+        await msg.edit(content="Sharing them on the internet...")
+        await asyncio.sleep(t)
+        await msg.edit(content=f"Looking for {player}'s \"Homework\" folder...")
+        await asyncio.sleep(t)
+        await msg.edit(content=f"Replacing all content with cute kitten images...")
+        await asyncio.sleep(t)
+        await msg.edit(content="Removing all traces...")
+        await asyncio.sleep(t)
+        await msg.edit(content=f"Successfully hacked {player}.")
 
 
 @bot.event
@@ -28,6 +55,12 @@ async def on_command_error(ctx, error):
     if isinstance(error, CommandNotFound) and not any(command in ctx.message.content for command in other_bot_commands):
         await ctx.send('This command is unknown. Try checking the spelling, or ask the dev to make it into the bot.')
     return error
+
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.me.name}")
+    # await bot.change_presence(presence=presence.ClientPresence(activities=presence.PresenceActivity(name="Minecraft")))
 
 
 @bot.event
